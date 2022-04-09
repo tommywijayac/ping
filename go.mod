7139fd2298e73f10993904fc77cdc6e84a6ff7b3
tommywijayac <tommy.wijaya.c@gmail.com>
2022-04-09 13:55:36 +0700

    func: add serial lib

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/tommywijayac/ping
 
 go 1.17
+
+require (
+	github.com/tarm/serial v0.0.0-20180830185346-98f6abe2eb07 // indirect
+	golang.org/x/sys v0.0.0-20220406163625-3f8b81556e12 // indirect
+)
--- internal/repo/serial/listen.go
+++ internal/repo/serial/listen.go
@@ -1,14 +1,30 @@
 package serial
 
 import (
+	"bufio"
 	"fmt"
 	"math/rand"
 	"time"
+
+	tarmserial "github.com/tarm/serial"
 )
 
 //Listen is a blocking function that continuously listen to a serial port and push received data to channel
 //TODO: must handle SIGTERM
-func (s *Serial) Listen(port string) {
+func (s *Serial) Listen(port string) error {
+	stream, err := tarmserial.OpenPort(&tarmserial.Config{
+		Name: "", //COMxx in Windows or /dev/tty in Linux
+		Baud: 9600,
+		Size: 8,
+	})
+	if err != nil {
+		return fmt.Errorf("fail to open serial port: %w", err)
+	}
+
+	scanner := bufio.NewScanner(stream)
+	//TODO: listen to actual scanner
+	fmt.Println(scanner)
+
 	for {
 		//TODO: change into real serial listener
 		data := rand.Intn(100)
