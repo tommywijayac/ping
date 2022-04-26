610742240de2498478123bac61f6a42d3274f171
tommywijayac <tommy.wijaya.c@gmail.com>
2022-04-26 21:23:32 +0700

    feat: FE websocket

--- client/ping/src/App.vue
+++ client/ping/src/App.vue
@@ -1,15 +1,58 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+	<div>
+    <RoomCard 
+      v-for="(room, index) in rooms"
+      :title="room.title"
+      :index="index"
+      :key="room.id" />
+    <button v-on:click="sendMessage('hello')">Send Message</button>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import RoomCard from './components/RoomCard.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    RoomCard
+  },
+  data: function() {
+    return {
+      rooms: {},
+      connection: null
+    }
+  },
+  methods: {
+    sendMessage: function(message) {
+      this.connection.send(message);
+    }
+  },
+  created: function(){
+    console.log("Starting connection to WebSocket Server")
+    this.connection = new WebSocket("ws://localhost:3000/ping")
+
+    //store app context
+    const app = this
+
+    this.connection.onmessage = function(event) {
+      var msg = JSON.parse(event.data)
+      var rooms = []
+
+      for (var i = 0; i < msg.length; i++) {
+        var room = {}
+        room.id = msg[i].id
+        room.title = msg[i].title
+        rooms.push(room)
+      }
+
+      app.rooms = rooms
+    }
+
+    this.connection.onopen = function(event) {
+      console.log(event)
+      console.log("Successfully connected to the echo websocket server...")
+    }
   }
 }
 </script>
