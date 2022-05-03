8d3b0b9ae3a3de10d1eb0eddaeff04a55baf2fc4
tommywijayac <tommy.wijaya.c@gmail.com>
2022-05-03 21:32:32 +0700

    check animation

--- client/ping/src/App.vue
+++ client/ping/src/App.vue
@@ -1,11 +1,16 @@
 <template>
-	<div>
-    <RoomCard 
-      v-for="(room, index) in rooms"
-      :title="room.title"
+	<div class="grid">
+    <RoomCard v-for="(room, index) in rooms"
       :index="index"
-      :key="room.id" />
-    <button v-on:click="sendMessage('hello')">Send Message</button>
+      :key="room.id"
+      :title="room.title"
+      :state="room.state"
+      @pingAck="onPingAck(room, room.id)"
+    />
+    <!-- <RoomCard :state="'active'" :title="'test'" :key="333" :id='333'
+      @pingAck="onPingAck(room)"
+    /> -->
+    <button v-on:click="sendMessage('hello')">Dummy trigger (simulate BE trigger)</button>
   </div>
 </template>
 
@@ -15,18 +20,57 @@ import RoomCard from './components/RoomCard.vue'
 export default {
   name: 'App',
   components: {
-    RoomCard
+    RoomCard,
   },
-  data: function() {
+  data() {
     return {
-      rooms: {},
+      rooms: [{
+        id: 0,
+        title: '<placeholder-title>',
+        state: 'active',
+      },{
+        id: 1,
+        title: '<placeholder-title>',
+        state: 'warning',
+      },{
+        id: 2,
+        title: '<placeholder-title>',
+        state: '',
+      },{
+        id: 3,
+        title: '<placeholder-title>',
+        state: 'active',
+      },{
+        id: 4,
+        title: '<placeholder-title>',
+        state: '',
+      }],
       connection: null
+    }
+  },
+  watch: {
+    rooms: {
+      handler(newValue, oldValue) {
+        console.log("hello from watcher", newValue, oldValue)
+      },
     }
   },
   methods: {
     sendMessage: function(message) {
       this.connection.send(message);
-    }
+    },
+    onPingAck: function(room, roomId) {
+      console.log(room);
+      console.log(roomId);
+      console.log(this.rooms);
+
+      // const toChangeRoom = this.rooms.find(r => r.id === roomId);
+      // toChangeRoom.state = '';
+
+      //this.rooms.push({id:333, title:"pushed from FE", state: ""})
+
+      //this.connection.send('hellaw from onPingAck');
+    },
   },
   created: function(){
     console.log("Starting connection to WebSocket Server")
@@ -37,16 +81,29 @@ export default {
 
     this.connection.onmessage = function(event) {
       var msg = JSON.parse(event.data)
-      var rooms = []
+      console.log(msg)
 
-      for (var i = 0; i < msg.length; i++) {
-        var room = {}
-        room.id = msg[i].id
-        room.title = msg[i].title
-        rooms.push(room)
-      }
+      //TODO: try mutate directly here
+      //Result: 
+      // var ref = msg[0].id
+      // const room = app.rooms.find(r => r.id === ref)
+      // room.state = "active"
 
-      app.rooms = rooms
+      //TODO: try modifying by push
+      //Result: 
+      app.rooms.push({id:333, title:"pushed from FE", state: ""})
+
+      // var rooms = []
+
+      // for (var i = 0; i < msg.length; i++) {
+      //   var room = {}
+      //   room.id = msg[i].id
+      //   room.title = msg[i].title
+      //   room.state = msg[i].state
+      //   rooms.push(room)
+      // }
+
+      // app.rooms = rooms
     }
 
     this.connection.onopen = function(event) {
@@ -58,12 +115,28 @@ export default {
 </script>
 
 <style>
+body {
+  height: 100vh;
+  box-sizing: border-box;
+  margin: 0px !important; /* override vue default 8px margin */
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  height: 100%;
+  box-sizing: border-box;
+  padding: 3%;
+}
+
+/*****************************************************/
+/********************* LAYOUT ************************/
+.grid {
+  display: grid;
+  gap: 30px 30px;
+  grid-template-columns: repeat(3, 1fr);
+  grid-template-rows: repeat(3, 1fr);
+  height: 100%;
 }
 </style>
--- client/ping/src/components/RoomCard.vue
+++ client/ping/src/components/RoomCard.vue
@@ -1,9 +1,273 @@
+<template>
+  <div
+    ref='card'
+    :class="[
+      'card',
+      { 'card-pulse': localState === 'active' },
+      { 'card-pulse-warn': localState === 'warning' }
+    ]"
+    v-on="localState === 'active' || localState === 'warning' ? 
+      { click: pingAck } : {}"
+  >
+    <div ref='content' class='content'>
+      <span class="title">{{ title }}</span>
+      <span>image</span>
+      <span>time</span>
+    </div>
+    <div ref='checkmarkcontainer' class='checkmark_container' hidden>
+      <svg ref='checkmark' class="checkmark" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 52 52">
+        <circle class="checkmark__circle" cx="26" cy="26" r="25" fill="none"/>
+        <path class="checkmark__check" fill="none" d="M14.1 27.2l7.1 7.2 16.7-16.8"/>
+      </svg>
+    </div>
+  </div>
+</template>
+
 <script>
 export default {
-  props: ['title']
+  props: {
+    title: { required: true, type: String},
+    state: { default: '', type: String}
+  },
+  data() {
+    return {
+      localState: this.state,
+      
+      //flag to make sure only one animation is played at a time
+      isAnimeFinished: true,
+    }
+  },
+  watch: {
+    localState(oldState, newState) {
+      console.log("inside watcher")
+      if (oldState !== newState) {
+        console.log("changed..?")
+        this.localState = newState;
+      }
+    }
+  },
+  methods: {
+    pingAck: function(){
+      if (!this.isAnimeFinished) {
+        return
+      }
+      this.isAnimeFinished = false;
+
+      var card = this.$refs.card;
+      card.classList.remove('card-pulse');
+      card.classList.remove('card-pulse-warn');
+
+      var content = this.$refs.content;
+      content.classList.add('blur');
+
+      //trigger check animation
+      var check = this.$refs.checkmarkcontainer;
+      check.removeAttribute('hidden');
+
+      //easy approach: wait until we're sure all animation are done
+      //with big enough value
+      setTimeout(this.pingAckAnimationFinish, 2500);
+      
+      //notify server
+      this.$emit('pingAck');
+    },
+    pingAckAnimationFinish: function() {
+      //clean up class
+      var card = this.$refs.card;
+      card.classList.remove('card-transition')
+
+      var content = this.$refs.content;
+      content.classList.remove('blur')
+
+      var check = this.$refs.checkmarkcontainer;
+      check.setAttribute('hidden', 'hidden');
+
+      //enable animation
+      this.isAnimeFinished = true;
+    }
+  }
 }
 </script>
 
-<template>
-  <h4>{{ title }}</h4>
-</template>
+<style scoped>
+/*****************************************************/
+/********************* CARD **************************/
+.card {
+  border: 1px solid black;
+  background-color: #333333;
+
+  /* disable text selection */
+  -moz-user-select: none;
+  -ms-user-select: none;
+  user-select: none;
+
+  /* configure elements to be centered vertically & horizontally */
+  /* need to do here as well (not only in content)*/
+  align-items: center;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  
+  /* so we can place div.check on top of div.content*/
+  position: relative;
+
+  transition: background-color 0.2s linear;
+}
+
+.card-pulse {
+  background-color: white;
+  box-shadow: 0 0 0 0 rgba(0, 81, 255, 1);
+  animation: pulse 1s infinite;
+}
+
+.card-pulse-warn {
+  background-color: rgba(255, 0, 0, 0.3);
+  box-shadow: 0 0 0 0 rgba(255,0 ,0, 1);
+  animation: pulse-warn 1s infinite;
+}
+
+@keyframes pulse {
+  0% {
+    box-shadow: 0 0 0 0 rgba(0, 81, 255, 0.7);
+  }
+
+  70% {
+    box-shadow: 0 0 0 10px rgba(0, 0, 0, 0);
+  }
+
+  100% {
+    box-shadow: 0 0 0 0 rgba(0, 0, 0, 0);
+  }
+}
+
+@keyframes pulse-warn {
+  0% {
+    box-shadow: 0 0 0 0 rgba(255, 0, 0, 0.7);
+  }
+
+  70% {
+    box-shadow: 0 0 0 10px rgba(0, 0, 0, 0);
+  }
+
+  100% {
+    box-shadow: 0 0 0 0 rgba(0, 0, 0, 0);
+  }
+}
+
+</style>
+
+<style scoped>
+/*****************************************************/
+/******************* COMPONENT ***********************/
+/* local variable */
+* {
+  --checkmark_color: #03a629;
+  --checkmark_size: 150px;
+}
+
+.content {
+  /* configure elements to be centered vertically & horizontally */
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  box-sizing: border-box;
+  margin: 1rem;
+}
+
+.checkmark_container {
+  /* position at parent's center */
+  /* https://stackoverflow.com/questions/2941189/how-to-overlay-one-div-over-another-div */
+  /* https://stackoverflow.com/questions/15328416/position-by-center-point-rather-than-top-left-point */
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%)
+}
+
+.checkmark__circle {
+  stroke-dasharray: 166;
+  stroke-dashoffset: 166;
+  stroke-width: 2;
+  stroke-miterlimit: 10;
+  stroke: var(--checkmark_color);
+  fill: none;
+  animation: stroke-circle 1.2s cubic-bezier(0.65, 0, 0.45, 1) forwards;
+}
+
+.checkmark {
+  width: var(--checkmark_size);
+  height: var(--checkmark_size);
+  border-radius: 50%;
+  display: block;
+  stroke-width: 2;
+  stroke: #fff;
+  stroke-miterlimit: 10;
+  margin: 10% auto;
+  box-shadow: inset 0px 0px 0px var(--checkmark_color);
+  animation: fill 1.6s ease-in-out .4s forwards, scale .3s ease-in-out .9s both;
+}
+
+.checkmark__check {
+  transform-origin: 50% 50%;
+  stroke-dasharray: 48;
+  stroke-dashoffset: 48;
+  animation: stroke-check 1s cubic-bezier(0.65, 0, 0.45, 1) 0.8s forwards;
+}
+
+@keyframes stroke-circle {
+  50% {
+    stroke-dashoffset: 0;
+  }
+  100% {
+    stroke-dashoffset: 166;
+  }
+}
+@keyframes stroke-check {
+  40%, 80% {
+    stroke-dashoffset: 0;
+  }
+  100% {
+    stroke-dashoffset: 48;
+  }
+}
+@keyframes scale {
+  0%, 100% {
+    transform: none;
+  }
+  50% {
+    transform: scale3d(1.1, 1.1, 1);
+  }
+}
+@keyframes fill {
+  50% {
+    box-shadow: inset 0px 0px 0px var(--checkmark_size) var(--checkmark_color);
+  }
+  100% {
+    box-shadow: inset 0px 0px 0px 0px var(--checkmark_color);
+  }
+}
+
+/* https://stackoverflow.com/questions/11977103/blur-effect-on-a-div-element */
+.blur {
+  filter: blur(0px);
+  animation: blur 2s linear;
+
+  /* transition duration & keyframes is tightly-tied to check animation: */
+  /* make sure it triggers after check vanishes, but before class is removed */
+}
+@keyframes blur {
+  0%, 100% {
+    filter: blur(0px);
+  }
+  10%, 80% {
+    filter: blur(5px);
+  }
+}
+
+.title {
+  font-size: 3rem;
+  font-family: Arial, Helvetica, sans-serif;
+}
+</style>
